18057d85916b4063b78555552c73acf6db61c08a
Manas Ranjan Biswal <biswalmanas345@gmail.com>
2022-01-01 19:42:51 +0530

    Adding Database

--- app/src/main/java/com/jonny/myspendings/MainActivity.java
+++ app/src/main/java/com/jonny/myspendings/MainActivity.java
@@ -2,13 +2,33 @@ package com.jonny.myspendings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText Reason,Amount;
+    Button AddList,WatchList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Reason = findViewById(R.id.Reason);
+        Amount = findViewById(R.id.Amount);
+
+        AddList = findViewById(R.id.AddList);
+        WatchList = findViewById(R.id.WatchList);
+
+
+        WatchList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,WatchClass.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/jonny/myspendings/WatchClass.java
@@ -0,0 +1,14 @@
+package com.jonny.myspendings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WatchClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_watch_class);
+    }
+}
